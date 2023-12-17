61dcf1a3c6399a6999351887de0b69202c5ca8de
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2023-12-17 09:20:03 +0000

    Minor change

--- lua/pavel/init.lua
+++ lua/pavel/init.lua
@@ -1,4 +1,4 @@
 require("pavel.remap")
 require("pavel.set")
 vim.cmd("colorscheme tokyonight-storm")
-vim.keymap.set("n", "<C-1>", function() end)
+vim.keymap.set("n", "<C->","<Cmd>Neotree toggle<CR>")
