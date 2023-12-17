2e1e73bcb5b345e69d724f7f81eb4ae7b8fa0c87
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2023-12-17 10:10:15 +0000

    Minor change

--- lua/pavel/set.lua
+++ lua/pavel/set.lua
@@ -18,7 +18,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "s"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
