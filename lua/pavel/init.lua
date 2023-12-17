0ad1c0f06b09417c34051d0c990dc784af4f7da0
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2023-12-17 10:30:17 +0000

    file name is now relative

--- lua/pavel/init.lua
+++ lua/pavel/init.lua
@@ -2,4 +2,13 @@ require("pavel.remap")
 require("pavel.set")
 vim.cmd("colorscheme tokyonight-storm")
 vim.keymap.set("n", "<C-a>","<Cmd>Neotree toggle<CR>")
-require('lualine').setup({})
+require('lualine').setup({
+  sections = {
+    lualine_c = {
+      {
+        "filename",
+        path = 1
+      }
+    }
+  }
+})
