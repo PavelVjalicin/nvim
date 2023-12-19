275e86a7f9af50c73dc4610702a89327411f53d0
Pavel Vjalicin <pavelVjalicin@users.noreply.github.com>
2023-12-19 02:35:06 +0000

    remove transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 vim.cmd("colorscheme tokyonight-storm")
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
